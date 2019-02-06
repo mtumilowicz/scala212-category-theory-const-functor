13859237ec4ce9c6a8f40ca7bee41032b10245a7
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-06 19:40:46 +0100

    tests

--- src/test/scala/ConstTest.scala
+++ src/test/scala/ConstTest.scala
@@ -6,7 +6,11 @@ import org.scalatest.FunSuite
 class ConstTest extends FunSuite {
 
   test("testMap") {
-    
+    val const1: Const[String, Int] = Const(1)
+    val const2: Const[Object, Int] = Const(2)
+    println(const1)
+    println(const2)
+    val const3: Const[String, String] = const1.map(_ => String)
   }
 
 }
