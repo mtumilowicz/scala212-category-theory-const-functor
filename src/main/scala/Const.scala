ed4c77911146fa545790f545f21bf4f7f354d7c6
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-06 20:12:01 +0100

    naming

--- src/main/scala/Const.scala
+++ src/main/scala/Const.scala
@@ -1,6 +1,6 @@
 /**
   * Created by mtumilowicz on 2019-02-05.
   */
-final case class Const[C, A](const: A) {
+final case class Const[C, A](param: A) {
   def map[B](f: A => B): Const[C, B] = this.asInstanceOf[Const[C, B]]
 }
