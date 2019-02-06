9f1d600f98960a731b6935adafb9e99e4a21ca80
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-06 19:40:35 +0100

    const as a case class

--- src/main/scala/Const.scala
+++ src/main/scala/Const.scala
@@ -1,6 +1,6 @@
 /**
   * Created by mtumilowicz on 2019-02-05.
   */
-class Const[C, A] {
+final case class Const[C, A](const: A) {
   def map[B](f: A => B): Const[C, B] = this.asInstanceOf[Const[C, B]]
 }
