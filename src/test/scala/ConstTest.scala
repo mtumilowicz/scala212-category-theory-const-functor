756b37252611c3e6a7d466b4b04f6225df900498
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-06 20:31:50 +0100

    const (map fix) + tests

--- src/test/scala/ConstTest.scala
+++ src/test/scala/ConstTest.scala
@@ -1,16 +1,38 @@
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Matchers}
 
 /**
   * Created by mtumilowicz on 2019-02-05.
   */
-class ConstTest extends FunSuite {
+class ConstTest extends FunSuite with Matchers {
 
-  test("testMap") {
-    val const1: Const[String, Int] = Const(1)
-    val const2: Const[Object, Int] = Const(2)
-    println(const1)
-    println(const2)
-    val const3: Const[String, String] = const1.map(_ => String)
+  test("creation") {
+    val const1: Const[Int, String] = Const(1)
+    val const2: Const[Int, Object] = Const(1)
+
+    const1.param should be(1)
+    const2.param should be(1)
+  }
+
+  test("equals: same values, different dropped types") {
+    val const1: Const[Int, String] = Const(1)
+    val const2: Const[Int, Object] = Const(1)
+
+    const1 should be(const2)
+  }
+
+  test("equals: different values") {
+    val const1: Const[Int, String] = Const(1)
+    val const2: Const[Int, String] = Const(2)
+
+    const1 should not be const2
+  }
+
+  test("map") {
+    val const: Const[Int, Int] = Const(1)
+
+
+    val mapped: Const[Int, String] = const.map(_ => "a")
+    mapped.param should be(1)
   }
 
 }
